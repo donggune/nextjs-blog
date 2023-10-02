a588f90814a80822c9de5d7e4014954a1d361ba9
lion <rhkwkdkrl@gmail.com>
2023-10-02 19:38:50 +0900

    0.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Blog Start !
-    </main>
+    <section className="bg-gray-500">
+      Main
+    </section>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-slate-900 text-white py-2 text-sm text-center'>{"All Right Reserved."}</div>
+  )
+}
