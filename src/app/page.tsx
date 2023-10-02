47c73ff7882fcf45ff39153b9364c811197503a4
lion <rhkwkdkrl@gmail.com>
2023-10-02 21:08:09 +0900

    Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <section className="bg-gray-500">
-      Main
-    </section>
+    <>
+      <Hero />
+      <FeaturePosts />
+    </>
   )
 }
