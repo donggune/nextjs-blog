a960cbfdda43dbbc9b9911d9cf17f28b5fb23e34
lion <rhkwkdkrl@gmail.com>
2023-10-02 20:24:55 +0900

    0.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import FeaturePosts from "@/components/FeaturePosts";
+import Hero from "@/components/Hero";
+
 export default function HomePage() {
   return (
-    <section className="bg-gray-500">
-      Main 5
-    </section>
+    <>
+      <Hero />
+      <FeaturePosts />
+    </>
   )
 }
