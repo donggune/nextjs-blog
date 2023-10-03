174ee1b5c120ecc7011dc31a15e492d6383a3a14
lion <rhkwkdkrl@gmail.com>
2023-10-03 18:06:21 +0900

    code markdown delete

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,32 +2,16 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Image from 'next/image';
+
 
 export default function MarkdownViewer({content}: {content : string}) {
     return (
         <ReactMarkdown className='prose max-w-none lg:prose-xl'
             remarkPlugins={[remarkGfm]}
             components={{
-                code(props) {
-                  const {children, className, node, ...rest} = props
-                  const match = /language-(\w+)/.exec(className || '')
-                  return match ? (
-                    <SyntaxHighlighter
-                    language={match[1]}
-                    PreTag="div"
-                    {...rest}
-                    style={materialDark}
-                    >
-                        {String(children).replace(/\n$/, '')}
-                    </SyntaxHighlighter>
-                  ) : (
-                    <code {...rest} className={className}>
-                      {children}
-                    </code>
-                  )
-                },
+                
                 img : (image) => (
                     <Image 
                     className='w-full max-h-60 object-cover'
