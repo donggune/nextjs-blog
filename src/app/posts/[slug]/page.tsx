165d1014ef057b03998e8e881148a1ea141aa23f
lion <rhkwkdkrl@gmail.com>
2023-10-03 16:42:10 +0900

    post next prev

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { getPostData } from '@/app/service/posts';
-import MarkdownViewer from '@/components/MarkdownViewer';
+import AdjacentPostCard from '@/components/AdjacentPostCard';
+import PostContent from '@/components/PostContent';
 import Image from 'next/image';
 import React from 'react'
-import {AiTwotoneCalendar} from 'react-icons/ai';
 
 type Props = {
     params : {
@@ -14,23 +14,18 @@ export default async function PostPage({params:{slug}}: Props) {
     // 1.전달된 slug 에 해당하는 포스트 데이터를 읽어와서
     // 2.데이터를 마크다운뷰어에 표기하면 됨
 
-    const {title, description, date, path, content} = await getPostData(slug);
+    const post = await getPostData(slug);
+    const { title, path, next, prev} = post;
 
     return <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
-        <Image src={`/images/posts/${path}.png`} alt={title} 
+        <Image src={`/images/posts/${post.path}.png`} alt={post.title} 
         className='w-full h-1/5 max-h-[500px]'
         width={760}
         height={420}/>
-        <section className='flex flex-col p-4'>
-            <div className='flex items-center self-end text-sky-600'>
-                <AiTwotoneCalendar />
-                <p className='font-semibold ml-2'>{date.toString()}</p>
-            </div>            
-            <h1 className='text-4xl font-bold'>{title}</h1>
-            <p className='text-xl font-bold'>{description}</p>
-            <div className='w-44 border-2 border-sky-600 mt-4 mb-8'/>
-            <MarkdownViewer content={content}/>
+        <PostContent post={post} />
+        <section className='flex shadow-md'>
+            {prev && <AdjacentPostCard post={post} type="prev"/>}
+            {next && <AdjacentPostCard post={post} type="next"/>}
         </section>
-        
     </article>
 }
--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,42 @@
+'use client';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
 
 export default function MarkdownViewer({content}: {content : string}) {
     return (
-        <ReactMarkdown className='prose lg:prose-xl'
+        <ReactMarkdown className='prose max-w-none lg:prose-xl'
             remarkPlugins={[remarkGfm]}
+            components={{
+                code(props) {
+                  const {children, className, node, ...rest} = props
+                  const match = /language-(\w+)/.exec(className || '')
+                  return match ? (
+                    <SyntaxHighlighter
+                    language={match[1]}
+                    PreTag="div"
+                    {...rest}
+                    style={materialDark}
+                    >
+                        {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code {...rest} className={className}>
+                      {children}
+                    </code>
+                  )
+                },
+                img : (image) => (
+                    <Image 
+                    className='w-full max-h-60 object-cover'
+                    src={image.src || ''} 
+                    alt={image.alt || ''} 
+                    width={500} 
+                    height={350} />
+                ),
+              }}
         >{content}</ReactMarkdown>
     )
 }
