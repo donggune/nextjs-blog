5eec655192036946dccb6655c113117bfb859755
lion <rhkwkdkrl@gmail.com>
2023-10-04 23:00:40 +0900

    ContactForm

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import ContactForm from "@/components/ContactForm";
+import {AiFillGithub, AiFillLinkedin, AiFillYoutube} from "react-icons/ai";
+
+const LINKS = [
+  {icon : <AiFillGithub />, url : ''},
+  {icon : <AiFillLinkedin />, url : ''},
+  {icon : <AiFillYoutube />, url : ''},
+]
 
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>inifo@gmail.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a href={link.url} key={index} target="_blank"
+          className="text-5xl hover:text-yellow-400">{link.icon}</a>
+        ))} 
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Send me an email</h2>
+      <ContactForm />
+    </section>
   )
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+export type BannerData = {
+  message : string;
+  state : 'success' | 'error';
+}
+export default function Banner({
+  banner : {message, state}
+}: { 
+  banner : BannerData
+}) {
+
+  const isSuccess = state === 'success';
+  const icon = isSuccess ? '💕' : '🥲';
+  return (
+    <p className={`p-2 ${isSuccess ? 'bg-green-300' : 'bg-red-300'}`}>
+      {`${icon} ${message}`}
+    </p>
+  )
+}
